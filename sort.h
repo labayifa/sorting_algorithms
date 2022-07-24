a2e6d79f63633ec2f51d8c15b46092333c764103
Carmel Prosper SAGBO <sagbocarmel@gmail.com>
2022-07-24 01:54:36 +0100

    advanced

--- sort.h
+++ sort.h
@@ -30,5 +30,37 @@ void quick_sort(int *array, size_t size);
 
 void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2);
 
+void swap_datas(int *a, int *b);
+
+void shell_sort(int *array, size_t size);
+
+void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker);
+void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
+void cocktail_sort_list(listint_t **list);
+
+void counting_sort(int *array, size_t size);
+
+void merge_subarr(int *subarr, int *buff, size_t front, size_t mid,
+		size_t back);
+void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back);
+void merge_sort(int *array, size_t size);
+
+void max_heapify(int *array, size_t size, size_t base, size_t root);
+
+void heap_sort(int *array, size_t size);
+
+int get_max(int *array, int size);
+void radix_counting_sort(int *array, size_t size, int sig, int *buff);
+void radix_sort(int *array, size_t size);
+
+void bitonic_merge(int *array, size_t size, size_t start, size_t seq,
+		char flow);
+void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow);
+void bitonic_sort(int *array, size_t size);
+
+int hoare_partition(int *array, size_t size, int left, int right);
+void hoare_sort(int *array, size_t size, int left, int right);
+void quick_sort_hoare(int *array, size_t size);
+
 #endif
 
